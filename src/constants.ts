a91dc8c1c9494300ae137f5060f782c4d875aae8
amaanrbl33 <119481788+amaanrbl33@users.noreply.github.com>
2026-03-26 23:26:46 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,42 +1,24 @@
-import img1 from './icon.jpeg';
-import img2 from './img2.jpeg';
-import img3 from './img3.jpeg';
-import img4 from './img4.jpeg';
-import img4 from './img5.jpeg';
-import img4 from './img6.jpeg';
-
+import icon from './assets/icon.jpeg';
+import img2 from './assets/img2.jpeg';
+import img3 from './assets/img3.jpeg';
+import img4 from './assets/img4.jpeg';
+import img5 from './assets/img5.jpeg';
+import img6 from './assets/img6.jpeg';
 
 export const SCHOOL_NAME = "Jamia Madinatul Uloom";
+
 export const CONTACT_INFO = {
-  phone1: "+918770550001",
-  phone2: "+919713987316",
+  phone1: "+919713987316",
+  phone2: "+918770550001",
   email: "jamiamadinatululoomrojroo@gmail.com",
   address: "Rojroo, Distt. Vidisha (Madhya Pradesh), 464221"
 };
 
 export const IMAGES = [
-  {
-    url: icon,
-    caption: "Main Building Arches"
-  },
-  {
-    url: img2,
-    caption: "School Campus"
-  },
-  {
-    url: img3,
-    caption: "Campus Overview"
-  },
-  {
-    url: img4,
-    caption: "Prayer Hall Exterior"
-  },
-  {
-    url: img5,
-    caption: "School Facilities"
-  },
-  {
-    url: img6,
-    caption: "Classroom Block"
-  }
+  { url: icon, caption: "Main Building Arches" },
+  { url: img2, caption: "School Campus" },
+  { url: img3, caption: "Campus Overview" },
+  { url: img4, caption: "Prayer Hall Exterior" },
+  { url: img5, caption: "School Facilities" },
+  { url: img6, caption: "Classroom Block" }
 ];
