146d4486b801ed3bcbb0dd1d7f6bb89bd431b67b
amaanrbl33 <119481788+amaanrbl33@users.noreply.github.com>
2026-03-26 22:59:14 +0530

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,11 @@
+import img1 from './icon.jpeg';
+import img2 from './img2.jpeg';
+import img3 from './img3.jpeg';
+import img4 from './img4.jpeg';
+import img4 from './img5.jpeg';
+import img4 from './img6.jpeg';
+
+
 export const SCHOOL_NAME = "Jamia Madinatul Uloom";
 export const CONTACT_INFO = {
   phone1: "+919713987316",
@@ -8,27 +16,27 @@ export const CONTACT_INFO = {
 
 export const IMAGES = [
   {
-    url: "https://images.weserv.nl/?url=https://storage.googleapis.com/m-infra.appspot.com/v/a/0/8/084d5807-657e-402c-967a-18f4007889e4.png",
+    url: icon,
     caption: "Main Building Arches"
   },
   {
-    url: "https://images.weserv.nl/?url=https://storage.googleapis.com/m-infra.appspot.com/v/a/0/0/00388755-654a-449e-866b-4560751912f2.png",
+    url: img2,
     caption: "School Campus"
   },
   {
-    url: "https://images.weserv.nl/?url=https://storage.googleapis.com/m-infra.appspot.com/v/a/b/8/b854a243-7186-4638-958b-0e5a62319f39.png",
+    url: img3,
     caption: "Campus Overview"
   },
   {
-    url: "https://images.weserv.nl/?url=https://storage.googleapis.com/m-infra.appspot.com/v/a/a/4/a498453c-a901-460d-859e-10b24021703b.png",
+    url: img4,
     caption: "Prayer Hall Exterior"
   },
   {
-    url: "https://images.weserv.nl/?url=https://storage.googleapis.com/m-infra.appspot.com/v/a/d/4/d48083a2-520e-436d-9725-63467615965a.png",
+    url: img5,
     caption: "School Facilities"
   },
   {
-    url: "https://images.weserv.nl/?url=https://storage.googleapis.com/m-infra.appspot.com/v/a/9/2/92429649-165c-4315-99d9-93b59345c269.png",
+    url: img6,
     caption: "Classroom Block"
   }
 ];
